e15e1bb8bb1a27dc990e7070e718d8d6299c2eb0
ridwanf <ridwanfansuri@gmail.com>
2022-12-23 16:26:57 +0700

    add winner

--- contracts/arisan.sol
+++ contracts/arisan.sol
@@ -2,11 +2,17 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract Lottery {
+contract Arisan {
+
+    struct PlayerData {
+        string name;
+        string playerAddress;
+        uint256 balance;
+    }
     address public manager;
     address[] public players;
-    string[] private namePlayers;
-
+    PlayerData[] private winnerList;
+    PlayerData[] private listPlayers;
     constructor() {
         manager = msg.sender;
     }
@@ -16,10 +22,11 @@ contract Lottery {
         _;
     }
 
-    function enter(string memory _name) public payable {
+    function enter(string memory _name, string memory _address) public payable {
         require(msg.value > .01 ether, "");
+        PlayerData memory data = PlayerData(_name, _address, msg.value);
         players.push(msg.sender);
-        namePlayers.push(_name);
+        listPlayers.push(data);
     }
 
     function random() private view returns (uint256) {
@@ -31,19 +38,30 @@ contract Lottery {
             );
     }
 
-    function pickWinner() public onlyManager {
+    function pickWinner() public onlyManager returns (PlayerData memory){
         require(players.length > 0, "Please add the player first");
         uint256 index = random() % players.length;
+        PlayerData memory winner = PlayerData(listPlayers[index].name, listPlayers[index].playerAddress, address(this).balance);
         payable(players[index]).transfer(address(this).balance);
+        winnerList.push(winner);
         clearData();
+        return winner;
     }
 
-    function getPlayers() public view returns (string[] memory) {
-        return namePlayers;
+    function getPlayers() public view returns (PlayerData[] memory) {
+        return listPlayers;
+    }
+
+     function getWinnerList() public view returns (PlayerData[] memory) {
+        return winnerList;
     }
 
     function clearData() private {
         delete players;
-        delete namePlayers;
+        delete listPlayers;
+    }
+
+    function clearWinner() public onlyManager {
+        delete winnerList;
     }
 }
